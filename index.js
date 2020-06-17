e38950a0ea5fb9d1b74409d2edef9a8f042ee0dc
Romuald Nave <romu.nave@gmail.com>
2020-06-17 16:34:33 +0200

    Create web server

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 8080;
+
+app.use('/', express.static(path.join(__dirname)));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
